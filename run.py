c7ecfcd1f853a90014b7c19054fa0ea05bf000f2
amdaretejas <amdaretejas1@gmail.com>
2025-10-06 17:47:56 +0530

    model training done

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # run.py
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 # Call the function you want to test
 # initiate_data_ingestion()
@@ -8,4 +9,7 @@ obj=DataIngestion()
 train_data, test_data = obj.initiate_data_ingestion()
 
 data_transformation=DataTransformation()
-data_transformation.initiate_data_transformation(train_data, test_data)
+train_arr, test_arr, _=data_transformation.initiate_data_transformation(train_data, test_data)
+
+modeltrainer=ModelTrainer()
+print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
